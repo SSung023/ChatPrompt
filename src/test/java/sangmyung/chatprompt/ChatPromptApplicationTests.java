ed6a9bd8379d4df01c2209ea113f2a7df6585743
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-03-16 20:27:36 +0900

    [Feat] Create project

---
+++ src/test/java/sangmyung/chatprompt/ChatPromptApplicationTests.java
@@ -0,0 +1,13 @@
+package sangmyung.chatprompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPromptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
