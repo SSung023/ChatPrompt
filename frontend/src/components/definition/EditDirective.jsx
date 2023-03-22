b98ca725415ccd4a16cd23b541188ed43653094b
Kim Da Eun <midekuna@gmail.com>
2023-03-22 17:14:50 +0900

    [Docs] Remove definition button

--- frontend/src/components/definition/EditDirective.jsx
+++ frontend/src/components/definition/EditDirective.jsx
@@ -11,7 +11,7 @@ export default function EditDirective() {
     const [input1, setInput1] = useState('');
     const [input2, setInput2] = useState('');
 
-    const userId = GetUserId(context.state.data.name);
+    // const userId = GetUserId(context.state.data.name);
     const taskId = context.state.data.inst_taskId;
     const [taskNum, setTaskNum] = useState(taskId);
 
@@ -117,8 +117,8 @@ export default function EditDirective() {
                 </form>
             </div>
             <div className={styles.buttons}>
-                {(userId === 1 || userId === 2 || userId === 3) 
-                && <button onClick={saveInstruction}>교수님 전용 윤문 수정 버튼</button>}
+                {/* {(userId === 1 || userId === 2 || userId === 3) 
+                && <button onClick={saveInstruction}>교수님 전용 윤문 수정 버튼</button>} */}
                 {/* <button onClick={handleLoad}>저장 없이 왼쪽 지정 페이지로 이동</button> */}
                 <button onClick={handleSaveAndLoad}>저장하고 다음 페이지로 이동</button>
             </div>
