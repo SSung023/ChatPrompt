01a16c371fe40227da45fa1a3d9754ff12895ee3
Kim Da Eun <midekuna@gmail.com>
2023-03-23 02:40:24 +0900

    [Feat] Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,7 @@
 import axios from 'axios';
-import React, { useContext } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import SelectBox from '../components/ui/selectbox/SelectBox';
-import { userContext } from '../context/UserContext';
-import SignIn from '../utility/SignIn';
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { SET_NAME, userContext } from '../context/UserContext';
 import styles from './Login.module.css';
 
 export default function Login() {
@@ -15,31 +13,80 @@ export default function Login() {
         justifyContent: `center`,
     }
 
-    const writer = ["박소영", "김다은", "성희연", "홍길동", "권경란"];
+    const [identifier, setIdentifier] = useState(0);
+    const [annotator, setAnnotator] = useState('');
+    const writers = ["박소영", "김다은", "성희연", "홍길동", "권경란"];
     const context = useContext(userContext);
-    const name = context.state.data.name;
+    // const name = context.state.data.name;
 
     const navigate = useNavigate();
-    const handleLogin = () => {
-        axios.post(`/api/login?username=${name}`)
-        .then(function(res) {
-            // console.log(res);
+    const handleLogin = (e) => {
+        e.preventDefault();
+        if(!annotator){
+            alert('이름을 입력하세요.');
+            return;
+        }
+        if(!writers.includes(annotator)){
+            alert('이름을 확인해주세요.');
+            return;
+        }
+        if(writers.indexOf(annotator) !== parseInt(identifier)){
+            alert('아이디를 확인해주세요.');
+            return;
+        }
+        
+        axios.post(`/api/login?username=${annotator}`)
+        .then(function() {
+            context.actions.contextDispatch({ type: SET_NAME, data: annotator })
         })
-        .then(function(res) {
-            window.localStorage.setItem('name', name);
+        .then(function() {
+            window.localStorage.setItem('name', annotator);
             navigate('/');
+        })
+        .catch(function(err) {
+            // 로그인 요청에 실패한 경우
+            alert('잠시 후에 다시 시도해주세요.');
         })
     }
 
     return (
         <div className='body' style={bodyStyle}>
             <div className={styles.wrapper}>
-                <div className={styles.start}>
-                    <SelectBox defaultValue="구축자 선택" options={writer}/>
-                    
-                    <h1>{`님, 안녕하세요!`}</h1>
-                </div>
-                <button 
+                <form
+                    onSubmit={handleLogin}
+                    className={styles.loginForm}
+                >
+                    <label>ID:</label>
+                    <select
+                        name = "annotator" 
+                        onChange={(e) => {
+                            setIdentifier(e.target.value);
+                        }}
+                        value={identifier}
+                    >
+                        <option value={0} defaultValue>A</option>
+                        <option value={1}>B</option>
+                        <option value={2}>C</option>
+                        <option value={3}>D</option>
+                        <option value={4}>E</option>
+                        <option value={5}>F</option>
+                        <option value={6}>G</option>
+                    </select>
+
+                    <label>Name:</label>
+                    <input
+                        type="text" 
+                        name="name"
+                        value={annotator}
+                        onChange={(e) => {
+                            setAnnotator(e.target.value);
+                        }}
+                        placeholder='이름'
+                        required
+                        autoComplete='off'
+                    />
+                </form>
+                <button
                     className={styles.button} 
                     onClick={handleLogin}
                 >{`시작하기 >`}</button>
