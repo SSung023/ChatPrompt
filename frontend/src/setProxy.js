098c30e80af29c0277a3be8db08f29d31be0a536
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-04-03 10:27:50 +0900

    [Build] Change FE build files

--- frontend/src/setProxy.js
+++ frontend/src/setProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// 13.125.187.231
+// 43.200.176.184
 module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://13.125.187.231:8080',	// 서버 URL or localhost:설정한포트번호
+            target: 'http://43.200.176.184:8080',	// 서버 URL or localhost:설정한포트번호
             changeOrigin: true,
         })
     );
