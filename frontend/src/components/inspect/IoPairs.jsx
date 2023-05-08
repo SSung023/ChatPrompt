268a3e051a2a3f9a288ef1b16e7d55e6d6644405
Kim Da Eun <midekuna@gmail.com>
2023-05-08 20:41:28 +0900

    [Feat] Add modify hot key button at inspect page

--- frontend/src/components/inspect/IoPairs.jsx
+++ frontend/src/components/inspect/IoPairs.jsx
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { userContext } from '../../context/UserContext';
-import Table, { TableBody, TableRow } from '../ui/table/Table';
+import Table, { TableBody } from '../ui/table/Table';
 import IoInspect from './IoInspect';
+import styles from './IoPairs.module.css';
 
 export default function IoPairs() {
+    const navigate = useNavigate();
+
     const context = useContext(userContext);
 
     // store 정보
@@ -45,6 +49,13 @@ export default function IoPairs() {
                     <IoInspect data={io} idx={idx}/>
                 </TableBody>
             </Table>
+
+            <p 
+                className={styles.modifyBtn}
+                onClick={() => {
+                    navigate('../input');
+                }}
+            >{`입출력 수정하러 가기 >`}</p>
         </div>
     );
 }
