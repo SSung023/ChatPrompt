18d50f249ef676ab4116992c6322213c9a8eca91
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-05-18 12:07:46 +0900

    [Feat] Add check input/output duplicate

--- src/main/java/sangmyung/chatprompt/assignment/repository/AssignmentRepository.java
+++ src/main/java/sangmyung/chatprompt/assignment/repository/AssignmentRepository.java
@@ -48,7 +48,7 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     Optional<Assignment> extractOfficialInstruct(@Param("taskId") Long taskId);
 
 
-    @Query("select a from Assignment a where a.user.id = 3L or a.user.id = 4L or a.user.id = 5L or a.user.id = 6L" +
+    @Query("select a from Assignment a where (a.user.id = 3L or a.user.id = 4L or a.user.id = 5L or a.user.id = 6L)" +
             "and a.input is not null")
     List<Assignment> findAllAssignment();
 }
